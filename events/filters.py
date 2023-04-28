3c0a012e23333bc3f31a20e951d449190153145f
MeldSnake <ikeek.33@gmail.com>
2023-04-28 14:20:05 +0200

    Added filter of events by status name.
    Signed-off-by: MeldSnake <ikeek.33@gmail.com>

--- events/filters.py
+++ events/filters.py
@@ -4,7 +4,7 @@ from extras.filters import CustomDateFilter
 
 
 class EventFilterSet(filters.FilterSet):
-    # status = filters.ChoiceFilter()
+    status = filters.CharFilter(method='status_filter')
     attendees = filters.RangeFilter()
     event_date_at = CustomDateFilter(field_name="event_date")
     event_date = filters.DateFromToRangeFilter()
@@ -21,3 +21,8 @@ class EventFilterSet(filters.FilterSet):
             "event_date",
             "no_contact",
         ]
+
+    def status_filter(self, queryset, fieldname, value):
+        return queryset.filter(
+            event_status__name=value,
+        )
