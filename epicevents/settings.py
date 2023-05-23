5a5c95cdb11e6f789fa15daec6f3912199ecaa23
MeldSnake <ikeek.33@gmail.com>
2023-05-23 16:59:34 +0200

    Typo in AUTH_PASSWORD_VALIDATORS.
    Signed-off-by: MeldSnake <ikeek.33@gmail.com>

--- epicevents/settings.py
+++ epicevents/settings.py
@@ -110,19 +110,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": (
-            "django.contrib.auth.password_validation"
+            "django.contrib.auth.password_validation."
             "MinimumLengthValidator"
         ),
     },
     {
         "NAME": (
-            "django.contrib.auth.password_validation"
+            "django.contrib.auth.password_validation."
             "CommonPasswordValidator"
         ),
     },
     {
         "NAME": (
-            "django.contrib.auth.password_validation"
+            "django.contrib.auth.password_validation."
             "NumericPasswordValidator"
         ),
     },
